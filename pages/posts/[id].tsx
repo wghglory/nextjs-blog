79fc06aad197c018195359b792f50932830f6a0c
Guanghui Wang <guanghui-wang@foxmail.com>
2021-04-03 21:36:56 +0800

    feature/ts (#1)

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,36 +1,41 @@
-import Head from 'next/head';
-
-import Date from '../../components/date';
 import Layout from '../../components/layout';
-
 import { getAllPostIds, getPostData } from '../../lib/posts';
-
+import Head from 'next/head';
+import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false, // any paths not returned by getStaticPaths will result in a 404 page.
   };
-}
+};
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData,
     },
   };
-}
+};
 
-export default function Post({ postData }) {
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
