ec447c830c044746ec3e35c141c37f11251a4d7d
Guanghui Wang <guanghui-wang@foxmail.com>
2021-04-03 14:31:46 +0800

    feat: polishing layout, utils module css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import Layout from '../../components/layout';
 
@@ -12,11 +11,6 @@ export default function FirstPost() {
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href='/'>
-          <a>Back to home</a>
-        </Link>
-      </h2>
     </Layout>
   );
 }
