2e23c0f13bd63a63bce465d33e6d653ba98ef901
Guanghui Wang <guanghui-wang@foxmail.com>
2021-04-03 14:05:46 +0800

    feat: add metadata head title

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 // http://localhost:3000/posts/first-post
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
